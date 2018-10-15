0172e2c28e0e9094781ece6b83c565957565df6c
minamorl <minamorl@minamorl.com>
2018-10-15 23:48:40 +0900

    Todo example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,12 +1,27 @@
 import {Micox, Portal, html} from "micox"
 
 const portal = new Portal()
-const component = (portal: Portal) => html.div([
-  html.div(portal.get("text") || "default"),
-  html.input("").events({
-    "keyup": (ev: any) => portal.transfer(new Map([["text", ev.target.value]]))
-  })
-])
+portal.transfer(new Map([["lists", ["Task1", "Task2"]]]))
+
+const component = (portal: Portal) => {
+  const lists = Array.from(Array((portal.get("lists").length || 0)).keys())
+    .map(v => html.li("" + portal.get("lists")[v]))
+  const ul = html.ul(lists)
+  const listData = portal.get("lists")
+  lists.forEach((v, i) => v.events({
+    click: (ev) => portal.transfer(new Map([["lists", listData.splice(i, 1) && listData]]))
+  }))
+  return html.div([
+    html.h1("ToDo App"),
+    html.input("").events({
+      "keyup": (ev: any) => {
+        if (ev.key === 'Enter')
+          portal.transfer(new Map([["lists", [...portal.get("lists"), ...[ev.target.value]]]]))
+      }
+    }),
+    ul
+  ])
+}
 document.addEventListener("DOMContentLoaded", function(event) {
   const container = document.querySelector("#app")
   if(container) new Micox(portal, container).contains(component)
