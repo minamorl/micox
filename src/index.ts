4b2c969125816702989cd01569e8745eb3d2a65f
minamorl <minamorl@minamorl.com>
2018-10-16 00:26:09 +0900

    Small fix

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const patch = snabbdom.init([ // Init patch function with chosen modules
     require('snabbdom/modules/props').default, // for setting properties on DOM elements
     require('snabbdom/modules/style').default, // handles styling on elements with support for animations
     require('snabbdom/modules/eventlisteners').default, // attaches event listeners
+    require('snabbdom/modules/attributes').default,
 ])
 
 import {h} from "snabbdom/h"
